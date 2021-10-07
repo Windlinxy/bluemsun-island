d60f49e7ad866bb7d9bef2d2944b601b2dd42f77
windlinxy <windlinxy@163.com>
2021-10-07 11:43:31 +0800

    完善配置文件-2

--- src/main/java/com/bluemsun/island/controller/UserController.java
+++ src/main/java/com/bluemsun/island/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.bluemsun.island.controller;
 
+import com.bluemsun.island.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @RequestMapping(
+            method = RequestMethod.POST
+    )
+    public User register(){
+        return new User();
+    }
 }
